17e25750b84eeff1985d3a7b88acd9bfddae2a7d
tsinh <tsinh1998@gmail.com>
2023-09-13 21:01:49 +0700

    13.9.23

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,12 +45,58 @@
             type: 'iframe'
           });
         }
-      };
+    };
+
+    var counter = function () {
+      if ($(document.body).hasClass("counter-scroll")) {
+        var a = 0;
+        $(window).scroll(function () {
+          var oTop = $(".counter").offset().top - window.innerHeight;
+          if (a == 0 && $(window).scrollTop() > oTop) {
+            if ($().countTo) {
+              $(".counter")
+                .find(".number")
+                .each(function () {
+                  var to = $(this).data("to"),
+                    speed = $(this).data("speed");
+                  $(this).countTo({
+                    to: to,
+                    speed: speed,
+                  });
+                });
+            }
+            a = 1;
+          }
+        });
+      }
+    };
+
+    var flatAccordion = function (class1,class2) {
+      var args = { duration: 600 };
+
+      $(class2 + ' .toggle-title.active').siblings('.toggle-content').show();
+      $(class1 +' .toggle-title').on('click', function () {
+          $(class1 + ' ' + class2).removeClass('active');
+          $(this).closest(class2).toggleClass('active');
+
+          if( !$(this).is('.active') ) {
+              $(this).closest(class1).find('.toggle-title.active').toggleClass('active').next().slideToggle(args);
+              $(this).toggleClass('active');
+              $(this).next().slideToggle(args);
+          } else {
+              $(class1 + ' ' + class2).removeClass('active');
+              $(this).toggleClass('active');
+              $(this).next().slideToggle(args);
+          }     
+      });
+    };
 
     // Dom Ready
     $(function () {
         btnmenu();
         video();
+        counter();
+        flatAccordion('.flat-accordion','.flat-toggle');
     });
 
 })(jQuery);
